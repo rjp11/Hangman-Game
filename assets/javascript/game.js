425b4f9ac43d2a5100aa04f602742d3d9239437f
Ryan Parks <rjp@ryans-mbp.attlocal.net>
2017-12-04 22:04:01 -0600

    game functions for the first word. does not generate a new word when winning or guesses = 0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,90 +4,65 @@ var wins = 0;
 var letters = [];
 var blanks = [];
 var wrongs = [];
-
-
-
+var dash = "-";
 //set an if loop based on number of guesses remaining
 if (guess > 0) {
-//log the letter guessed
-
-//provide a list of hangman words to be guessed
-var allWords = [ "ryan", "jon", "parks", "katie", "nicole", "johnson"];
-
-//utilize random number generator to select one of the words
-var word = allWords[Math.floor(Math.random() * allWords.length)];
-//split the chosen hangman word into an array of letters
-var wordArray = word.split("");
-//set a variable for the number of letters in the chosen hangman word
-var numLetters = wordArray.length;
-		
-//for loop to push "-"" into the blanks array until the length matches the length of variable word
-for (i=0; i < numLetters; i++){
-	blanks.push("-")
+    //provide a list of hangman words to be guessed
+    var allWords = ["ryan", "jon", "parks", "katie", "nicole", "johnson"];
+    //utilize random number generator to select one of the words
+    var word = allWords[Math.floor(Math.random() * allWords.length)];
+    //split the chosen hangman word into an array of letters
+    var wordArray = word.split("");
+    //set a variable for the number of letters in the chosen hangman word
+    var numLetters = wordArray.length;
+    //for loop to push "-"" into the blanks array until the length matches the length of variable word
+    for (i = 0; i < numLetters; i++) {
+        blanks.push(dash);
+    }
+    document.getElementById("hangman").innerHTML = blanks.join("");
+    document.getElementById("guessNumber").innerHTML = guess;
+    console.log(word);
+    console.log(wordArray);
+    console.log(numLetters);
+    console.log(blanks);
+    console.log(wrongs);
+    document.onkeypress = function(event) {
+        if (event.keyCode > 96 && event.keyCode < 123) {
+            console.log(event);
+            var letter = event.key;
+            //check if the letter is in the word
+            if (wordArray.indexOf(letter) === -1) {
+                //if the letter is already in wrongs, don't add to wrongs or reduce guesses
+                if (wrongs.indexOf(letter) === -1) {
+                    //if letter isn't in word and not already guessed, display in the letters already guessed section
+                    wrongs.push(letter);
+                    console.log(wrongs);
+                    guess = guess - 1;
+                    console.log(guess);
+                    document.getElementById("guessLetter").innerHTML = wrongs.join("");
+                    document.getElementById("guessNumber").innerHTML = guess;
+                }
+            }
+            //replace the blank with the correct letter guessed
+            else {
+                for (j = 0; j < word.length; j++) {
+                    if (letter === word[j]) {
+                        blanks[j] = letter;
+                        console.log(blanks);
+                        document.getElementById("hangman").innerHTML = blanks.join("");
+                        document.getElementById("guessNumber").innerHTML = guess;
+                    }
+                }
+            }
+        };
+        if (word === blanks.join("")) {
+            wins = wins + 1;
+            document.getElementById("winNumber").innerHTML = `${wins}`;
+            guess = 0;
+            blanks = [];
+        }
+    }
+} else {
+    document.getElementById("winNumber").innerHTML = `${wins}`;
+    guess = 12;
 }
-
-document.getElementById("hangman").innerHTML = blanks.join("");
-document.getElementById("guessNumber").innerHTML = guess;
-		
-console.log(word);
-console.log(wordArray);
-console.log(numLetters);
-console.log(blanks);
-console.log(wrongs);
-	document.onkeyup = function(event){
-		console.log(event);
-		var letter = event.key;
-
-
-		//check if the letter is in the word
-		if (wordArray.indexOf(letter) === -1){
-
-		//if not, display in the letters already guessed section
-		//document.getElementByID("game").innerHTML = ' <h6> Letters already guessed: ${wrongs} '
-			wrongs.push(letter);
-			console.log(wrongs);
-			guess = guess-1;
-			console.log(guess);
-
-			document.getElementById("guessLetter").innerHTML = wrongs.join("");
-			document.getElementById("guessNumber").innerHTML = guess;
-			//if the letter is already in wrongs, don't add to wrongs or reduce guesses
-
-
-		}
-
-		//replace the blank with the correct letter guessed
-
-		else {
-			for (j=0; j < word.length; j++){
-				if (letter === word[j]) {
-					blanks[j] = letter;
-					
-					if (blanks.indexOf(letter) > -1) {
-					 
-					} else {
-						guess = guess - 1;
-					}
-					console.log(blanks);
-					document.getElementById("hangman").innerHTML = blanks.join("");
-					document.getElementById("guessNumber").innerHTML = guess;
-				}
-			}
-
-
-		}
-
-
-		};
-
-
-		}
-
-else {
-
-}
-
-
-		
-
-		
