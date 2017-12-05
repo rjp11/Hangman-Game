b048be342f6e230e21b39b37b0b4947d7e7450ae
Ryan Parks <rjp@ryans-mbp.attlocal.net>
2017-12-04 19:59:02 -0600

    added join() function to remove commas

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,12 @@ var letters = [];
 var blanks = [];
 var wrongs = [];
 
+
+
+//set an if loop based on number of guesses remaining
+if (guess > 0) {
+//log the letter guessed
+
 //provide a list of hangman words to be guessed
 var allWords = [ "ryan", "jon", "parks", "katie", "nicole", "johnson"];
 
@@ -19,22 +25,22 @@ var numLetters = wordArray.length;
 for (i=0; i < numLetters; i++){
 	blanks.push("-")
 }
+
+document.getElementById("hangman").innerHTML = blanks.join("");
+document.getElementById("guessNumber").innerHTML = guess;
 		
 console.log(word);
 console.log(wordArray);
 console.log(numLetters);
 console.log(blanks);
 console.log(wrongs);
-
-//set an if loop based on number of guesses remaining
-if (guess > 0) {
-//log the letter guessed
 	document.onkeyup = function(event){
 		console.log(event);
 		var letter = event.key;
 
+
 		//check if the letter is in the word
-		if (wordArray.indexOf("letter") === "-1"){
+		if (wordArray.indexOf(event.key) === "-1"){
 
 		//if not, display in the letters already guessed section
 		//document.getElementByID("game").innerHTML = ' <h6> Letters already guessed: ${wrongs} '
@@ -42,6 +48,9 @@ if (guess > 0) {
 			console.log(wrongs);
 			guess --;
 			console.log(guess);
+
+			document.getElementByID("guessLetter").innerHTML = wrongs.join("");
+			document.getElementById("guessNumber").innerHTML = guess;
 			//if the letter is already in wrongs, don't add to wrongs or reduce guesses
 
 
@@ -54,6 +63,7 @@ if (guess > 0) {
 				if (letter === word[j]) {
 					blanks[j] = letter;
 					console.log(blanks);
+					document.getElementById("hangman").innerHTML = blanks.join("");
 				}
 			}
 
